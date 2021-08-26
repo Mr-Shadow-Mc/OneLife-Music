5116661787ec79389d1a98635c79c3cdded3a7b1
Mr-Shadow-Mc <82217376+Mr-Shadow-Mc@users.noreply.github.com>
2021-08-26 09:47:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client({
   partials: ['MESSAGE', 'CHANNEL', 'REACTION']
 });
-const config = require('./config.json');
 const {
   loadCommands
 } = require('./utils/loadCommands');
