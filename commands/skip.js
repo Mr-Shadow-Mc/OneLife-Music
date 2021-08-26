6128d17c913962744c52a6a3e19e4361554c242b
Mr-Shadow-Mc <82217376+Mr-Shadow-Mc@users.noreply.github.com>
2021-08-26 10:14:01 +0200

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -1,16 +1,16 @@
 const Discord = require('discord.js')
 
-module.exports.run = async (bot, message, args) => {
+module.exports.run = async (client, message, args) => {
     if (!message.member.voice.channel) return message.channel.send(new Discord.MessageEmbed()
         .setColor("fff100")
         .setDescription('🥂・。Vous devez être dans un canal vocal pour utiliser cette commande.'));
 
-    let queue = await bot.distube.getQueue(message);
+    let queue = await client.distube.getQueue(message);
 
     if(queue) {
-        bot.distube.skip(message)
+        client.distube.skip(message)
 
-        message.channel.sendnew Discord.MessageEmbed()
+        message.channel.send(new Discord.MessageEmbed()
         .setColor("fff100")
         .setDescription('🥂・。Skip !'))
     } else if (!queue) {
